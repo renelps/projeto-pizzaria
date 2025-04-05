5bdbdd21ee325a2960fbfcefdab079d7e4f2e911
renelps <renanlps616@gmail.com>
2025-04-05 09:15:00 -0300

    config

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
-// Habilita o modo de depuração do Mongoose
-mongoose.set('debug', true);
-
-// Conecta ao MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('🔥 Conectado ao MongoDB'))
   .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
