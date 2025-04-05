e3e40f19f13291e6b0074782af1548ea19c22ca9
renelps <renanlps616@gmail.com>
2025-04-05 09:08:18 -0300

    config mongo

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
+// Habilita o modo de depuração do Mongoose
+mongoose.set('debug', true);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("🔥 Conectado ao MongoDB"))
-  .catch(err => console.error("Erro ao conectar ao MongoDB:", err));
-
+// Conecta ao MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('🔥 Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
 const pizzaRoutes = require('./routes/pizzas');
 app.use('/api/pizzas', pizzaRoutes);
