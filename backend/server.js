ea74ee6b43db6cd05e73c2c1f7802e70d22fdb79
renelps <renanlps616@gmail.com>
2025-04-07 17:14:01 -0300

    config pizzas.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
